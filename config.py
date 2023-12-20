c6dd18c51ae79389385f3efa7765bfa04b2cd0a3
CheeSuan <154388652+CheeSuan@users.noreply.github.com>
2023-12-20 08:38:24 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cn6zn2sn8sip.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tancheesuan-employee"
+customregion = "us-east-1"
+
